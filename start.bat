65af5b90fddf143e66074364029aad8d7b22ef58
Olivier Beauchemin <oli__games@hotmail.com>
2017-11-21 22:54:43 -0500

    modify bg and some css properties

---
+++ start.bat
@@ -0,0 +1,2 @@
+docker build -t slack-invite-automation .
+docker run -it --rm -e COMMUNITY_NAME="Trading crypto-monnaies" -e SLACK_URL="tradingcm.slack.com" -e LOCAL="fr" -e SLACK_TOKEN="xoxp-276610073447-275675411237-275388039184-6fc3ebfefc2fb94cbc2a92c49f969aae" -p 3000:3000 slack-invite-automation
